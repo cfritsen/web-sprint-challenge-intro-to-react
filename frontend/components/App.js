3d0c61cbb79fc25874a36f1fecd4e8d0189b1574
cfritsen <chrisfritsen@gmail.com>
2023-12-20 01:38:20 -0800

    Implemented data fetching and processing.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import axios from 'axios'
 import Character from './Character'
+import {useState, useEffect} from 'react'
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [rawPeople, setRawPeople] = useState(undefined)
+  const [planets, setPlanets] = useState(undefined)
+  const [people, setPeople] = useState(undefined)
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    let promise1 = axios.get(urlPlanets)
+    let promise2 = axios.get(urlPeople)
+
+    Promise.all([promise1, promise2]).then(results => {
+      const [planets, rawPeople] = results
+      setPlanets(planets.data)
+      setRawPeople(rawPeople.data)
+    })  
+  }, [])
+
+  useEffect(() => {
+    console.log('start')
+    if (rawPeople !== undefined && planets !== undefined){
+      let peoplePlanets = rawPeople.map(person => {
+        let homeworld;
+        planets.forEach(planet => {
+          if (planet.id === person.homeworld){
+            homeworld = planet
+          }
+        })
+        return {...person, homeworld}
+      })
+      setPeople(peoplePlanets)
+  }
+  }, [rawPeople, planets])
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
